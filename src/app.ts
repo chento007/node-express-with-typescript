4eb78923906c3e5cd8dfa201692bd497efd7e451
chento007 <chentochea2002@gmail.com>
2024-02-09 20:55:40 +0700

    update corse

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from './app/router';
-
+import corse from "cors"
 
 const app = express();
+
+app.use(corse())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
