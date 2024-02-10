9470d8c602966b4e2291a0005a29fa84ed88caa2
chento007 <chentochea2002@gmail.com>
2024-02-10 13:14:07 +0700

    push

--- src/app/router/auth.ts
+++ src/app/router/auth.ts
@@ -11,5 +11,5 @@ export default (router: express.Router) => {
     router.post('/api/auth/register', register);
     router.post("/api/auth/login", login);
     router.get("/api/auth/me", isAuthenicationedUser, getProfile)
-    router.post("/api/auth/refresh", isAuthenicationedUser, refresh);
+    router.post("/api/auth/refresh", refresh);
 };
